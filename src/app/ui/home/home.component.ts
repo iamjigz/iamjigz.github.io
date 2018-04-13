65b7f96d33445ef827689de2b8104589fdcb37e0
Jigno Alfred V. Venezuela <jignoalfred.venezuela@live.com>
2018-04-13 15:42:02 +0800

    Changed about to contact

--- src/app/ui/home/home.component.ts
+++ src/app/ui/home/home.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  HttpClient,
-  HttpErrorResponse,
-  HttpHeaders
-} from "@angular/common/http";
-import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -12,29 +6,11 @@ import { Observable } from "rxjs";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  private headers: HttpHeaders = new HttpHeaders();
 
-  constructor(private http: HttpClient) {
-    this.headers
-    .set("Content-Type", "application/json")
-    .append(
-      "Access-Control-Allow-Methods",
-      "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-    )
-    .append("Access-Control-Allow-Origin", "*")
-    .append(
-      "Access-Control-Allow-Headers",
-      "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method"
-    );
-   }
+  constructor() { }
 
   ngOnInit() {
-    this.getComic().subscribe(res => {
-      console.log(res)
-    }, err => console.log(err))
+
   }
 
-  private getComic(): Observable<any> {
-    return this.http.get('https://xkcd.com/info.0.json');
-  }
 }
