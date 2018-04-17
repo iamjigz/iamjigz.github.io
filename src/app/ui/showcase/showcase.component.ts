ecd7a4a585c19a3a4f839a00fd94f9326fe5035b
Jigno Alfred V. Venezuela <jignoalfred.venezuela@live.com>
2018-04-17 12:39:16 +0800

    Updated showcase component

--- src/app/ui/showcase/showcase.component.ts
+++ src/app/ui/showcase/showcase.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-showcase',
-  templateUrl: './showcase.component.html',
-  styleUrls: ['./showcase.component.scss']
+  selector: "app-showcase",
+  templateUrl: "./showcase.component.html",
+  styleUrls: ["./showcase.component.scss"]
 })
 export class ShowcaseComponent implements OnInit {
+  projects: Array<any>;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.projects = [
+      {
+        title: "Bucket List",
+        icon: "fa fa-plane",
+        category: "Travels",
+        desc:
+          "Create and plan your own bucket list of places you want to visit. Powered by Google Places API.",
+        image:
+          "https://source.unsplash.com/6_pFPo2YM9c/300x300",
+        link: "/showcase/bucketlist"
+      }
+    ];
   }
-
 }
